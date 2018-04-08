41d7e4239f9f55278692ec135a5871cf917e90a2
Ujjwal1966 <ujjwalshukla1711@gmail.com>
2018-04-09 00:08:29 +0530

    Initial commit

---
+++ app/src/main/java/earthquake/com/earthquakeapplication/app/earthquakewatcher/Util/Constants.java
@@ -0,0 +1,20 @@
+package earthquake.com.earthquakeapplication.app.earthquakewatcher.Util;
+
+import java.util.Random;
+
+/**
+ * Created by UjjwalNUtsav on 07-04-2018.
+ */
+
+public class Constants {
+
+    public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+    public static final int LIMIT = 30;
+
+
+    public static int randomInt(int max, int min) {
+        return new Random().nextInt(max - min) + min;
+
+    }
+
+}
